9129ca554669672ce0394c14525875c6c1c6129c
bullmoosedev <koleervine@gmail.com>
2022-08-18 14:32:39 -0600

    changed about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -21,28 +21,29 @@ function About() {
     <section>
       <div class="jumbotron">
         <div class="container">
-          <h1 class="display-3">Hello, world!</h1>
-          <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-          <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more »</a></p>
+          <h1 class="display-3">Hello World!</h1>
+          <p>My name is Kole Ervine, I'm an avid outdoorsman, learner and extrovert. I work on elucidating the problems that go unnoticed, forging insights on otherwise veiled obstacles and creating unparalleled solutions to issues both small and great alike. Amazing code starts with amazing minds, and I wish to be among them whilst working to create the best experience one can have when browsing and exploring the web! 
+
+When I'm not at my desk writing stupefying code or working with other formidable web-engineers, I can be found out and about in the breath-taking wild. When I'm out in the world, I'm either among the awe-inspiring beauty of nature or amongst the magnificent literature that has helped form the minds of those who became pinnacles of achievement in our combined histories.</p>
+          <p><a class="btn btn-primary btn-lg" href="https://www.linkedin.com/in/bullmoosedev/" target="_blank" rel="noopener noreferrer" role="button">Learn more »</a></p>
         </div>
       </div>
 
       <div class="container">
         <div class="row">
           <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+            <h2>Past</h2>
+            <p>I recently graduated from the University of Utah's full stack flex coding bootcamp. I learned things like React, Sql, noSql, graphQl, Node.js, Express.js, CSS, HTML and MongoDB. Im still quite inexperienced compared to others within the industry of IT, but I have a desire to learn and build with a team! </p>
+            <p><a class="btn btn-secondary" href="https://www.credly.com/badges/fdf929a1-7cd9-42db-b776-feac15910450/public_url" target="_blank" rel="noopener noreferrer" role="button">See my Credly Badge here! »</a></p>
           </div>
           <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+            <h2>Present</h2>
+            <p>Currently I'm looking for work and stil educating myself. Sources like CS50 from EdX, PluralSight, Udemy and TheOdinProject have been great resources as I continue to learn and grow.</p>
           </div>
           <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+            <h2>Future</h2>
+            <p>I really like alot of the designs and ideas behind Web3. The blockchain and decentralized currencies are incredibly interesting and I would love to achieve the knowledge and capability of being able to code things of this nature!</p>
+            <p><a class="btn btn-secondary" href="https://cosmos.network/" target="_blank" rel="noopener noreferrer" role="button">View a site I would love to code »</a></p>
           </div>
         </div>
       </div>
