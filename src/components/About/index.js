f953932e4cd9ea9215448220d5ec8f70649796d5
bullmoosedev <koleervine@gmail.com>
2022-09-06 13:27:38 -0600

    added spell check and fixed errors

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -17,16 +17,16 @@ function About() {
         <div class="row">
           <div class="col-md-4">
             <h2>Past</h2>
-            <p>I recently graduated from the University of Utah's full stack flex coding bootcamp. I learned things like React, Sql, noSql, graphQl, Node.js, Express.js, CSS, HTML and MongoDB. Im still quite inexperienced compared to others within the industry of IT, but I have a desire to learn and build with a team! </p>
+            <p>I recently graduated from the University of Utah's full stack flex coding boot-camp. I learned things like React, Sql, noSql, graphQl, Node.js, Express.js, CSS, HTML and MongoDB. I'm still quite inexperienced compared to others within the industry of IT, but I have a desire to learn and build with a team! </p>
             <p><a class="btn btn-secondary" href="https://www.credly.com/badges/fdf929a1-7cd9-42db-b776-feac15910450/public_url" target="_blank" rel="noopener noreferrer" role="button">See my Credly Badge here! »</a></p>
           </div>
           <div class="col-md-4">
             <h2>Present</h2>
-            <p>Currently I'm looking for work and stil educating myself. Sources like CS50 from EdX, PluralSight, Udemy and TheOdinProject have been great resources as I continue to learn and grow.</p>
+            <p>Currently I'm looking for work and still educating myself. Sources like CS50 from EdX, PluralSight, Udemy and TheOdinProject have been great resources as I continue to learn and grow.</p>
           </div>
           <div class="col-md-4">
             <h2>Future</h2>
-            <p>I really like alot of the designs and ideas behind Web3. The blockchain and decentralized currencies are incredibly interesting and I would love to achieve the knowledge and capability of being able to code things of this nature!</p>
+            <p>I really like many of the designs and ideas behind Web3. The blockchain and decentralized currencies are incredibly interesting and I would love to achieve the knowledge and capability of being able to code things of this nature!</p>
             <p><a class="btn btn-secondary" href="https://cosmos.network/" target="_blank" rel="noopener noreferrer" role="button">View a site I would love to code »</a></p>
           </div>
         </div>
